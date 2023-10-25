f728a79b27d0569ef491d82fbd8d94037d128a5a
vincentmilland <vincentmilland@gmail.com>
2023-10-25 15:25:06 -0400

    Added decode function and update main function

--- main.py
+++ main.py
@@ -8,8 +8,19 @@ def encode(password):
         i = i + 1
     return afterencoding
 
+def decode(encoded_password):
+    length = len(encoded_password)
+    i = 0
+    afterdecoding = ""
+    while i < length:
+        afterdecoding = afterdecoding + str((int(encoded_password[i]) - 3) % 10)
+        i = i + 1
+    return afterdecoding
 
 if __name__ == "__main__":
+
+    encoded = None  # Initialize encoded variable to store the encoded password
+    original = None  # Initialize original variable to store the original password
 
     while True:
         print("Menu")
@@ -26,6 +37,10 @@ if __name__ == "__main__":
             if encoded:
                 print("Your password has been encoded and stored!")
         elif option == '2':
-            pass
+            if encoded:  # Ensure a password has been encoded before attempting to decode
+                decoded = decode(encoded)
+                print(f"The encoded password is {encoded}, and the original password is {decoded}.")
+            else:
+                print("No password has been encoded yet.")
         elif option == '3':
             break
